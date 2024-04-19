fe9bd75f2f63665d2b1091d933b58d82b6e85169
Kuranasaki@M3Max <ironpan10@hotmail.com>
2024-04-19 13:48:22 +0700

    wip

--- packages/socket/index.ts
+++ packages/socket/index.ts
@@ -1,12 +1,12 @@
 import { Server } from "socket.io";
 import { App } from "uWebSockets.js";
-import type { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from "./types/socket";
+import type { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from "@shared/socket";
 
 const app = App();
-const io = new Server();
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>();
 
 io.attachApp(app);
-
+ 
 io.on("connection", (socket) => {
   socket.on("userTyping", (isTyping: boolean) => {
     console.log(isTyping);
