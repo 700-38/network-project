b38870e561c48905eed9b9ba9e6c0fbb4dcabf6c
TitorPs360 <TitorPs360Gamer@gmail.com>
2024-04-19 23:43:05 +0700

    feat(Frontend): :heavy_plus_sign: Add lottie

--- packages/web/src/app/(routes)/page.tsx
+++ packages/web/src/app/(routes)/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import loadingAnimation from '@assets/lotties/loading.json';
+import Lottie from 'lottie-react';
 import React from 'react';
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <Lottie animationData={loadingAnimation} />
+    </div>
+  );
 };
 
 export default Page;
