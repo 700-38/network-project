3dc949a145ca594a418caf3c66693c80d9b6d038
punyaphatsura <punyaphat.sura@gmail.com>
2024-04-20 16:41:03 +0700

    feat(Frontend): :sparkles: saperate picture profile in group chat

--- packages/web/src/app/_components/messageBubble.tsx
+++ packages/web/src/app/_components/messageBubble.tsx
@@ -1,7 +1,9 @@
 'use client';
 'use client';
 
-import React, { FC, useState } from 'react';
+import { RealmContext } from '@/context/realm';
+import { useRouter } from 'next/navigation';
+import React, { FC, useContext, useState } from 'react';
 
 import { IMessageProp } from '../../../../../shared/types/message';
 
@@ -20,15 +22,23 @@ const MessageBubble: FC<Props> = ({
   isClicked,
   setIsClicked,
 }) => {
+  const router = useRouter();
+
   const stickers = ['🌟', '💬', '🎉', '👍'];
-  if (thisMessage.sender === 'You')
+  const Realm = useContext(RealmContext);
+  const yourId = Realm.realm?.id || '';
+  if (yourId === '') {
+    router.push('/login');
+  }
+
+  if (thisMessage.sender === yourId)
     return (
       <div
         className={`mb-1 flex flex-col items-center justify-center transition-[padding] duration-300 ${
           isClicked === thisMessage.id
             ? 'pb-4'
             : nextMessage &&
-              nextMessage.sender === 'You' &&
+              nextMessage.sender === yourId &&
               nextMessage.timestamp - thisMessage.timestamp > 1000 * 60 * 2 &&
               'pb-2'
         }`}>
@@ -40,10 +50,10 @@ const MessageBubble: FC<Props> = ({
                 new Date(thisMessage.timestamp).toLocaleDateString()) ||
             isClicked === thisMessage.id ||
             !prevMessage
-              ? 'h-12'
+              ? 'h-10'
               : 'h-0 opacity-0'
           }
-           overflow-hidden transition-[height] duration-300`}>
+           flex items-center justify-center overflow-hidden transition-[height,opacity] duration-300`}>
           {new Date().toLocaleDateString() !== new Date(thisMessage.timestamp).toLocaleDateString()
             ? new Date(thisMessage.timestamp).getDate().toString().padStart(2, '0') +
               '/' +
@@ -62,7 +72,7 @@ const MessageBubble: FC<Props> = ({
             <div
               className={`transition-all duration-300 ${
                 prevMessage &&
-                prevMessage.sender === 'You' &&
+                prevMessage.sender === yourId &&
                 thisMessage.timestamp - prevMessage.timestamp <= 1000 * 60 * 15 && // 15 mins
                 !(isClicked === thisMessage.id) &&
                 prevMessage.type === 'text' &&
@@ -72,23 +82,29 @@ const MessageBubble: FC<Props> = ({
               } 
              ${
                nextMessage &&
-               nextMessage.sender === 'You' &&
+               nextMessage.sender === yourId &&
                nextMessage.timestamp - thisMessage.timestamp <= 1000 * 60 * 15 && // 15 mins
                !(isClicked === thisMessage.id) &&
                nextMessage.type === 'text' &&
                isClicked !== nextMessage.id
                  ? 'rounded-br-md'
                  : 'rounded-br-3xl'
-             } min-w-[40px] max-w-[400px] whitespace-pre-wrap break-words rounded-l-3xl bg-blue-300 px-3 py-2 text-center`}
-              onClick={() => setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id)}>
+             } min-w-[40px] max-w-[400px] whitespace-pre-wrap break-words rounded-l-3xl ${isClicked === thisMessage.id ? 'bg-blue-400' : 'bg-blue-300'} px-3 py-2 text-center`}
+              onClick={() => {
+                setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id);
+                console.log(thisMessage, prevMessage, nextMessage);
+              }}>
               {thisMessage.content}
             </div>
           ) : (
             <div className={`flex w-full justify-end text-6xl`}>
               <div
                 className="my-2"
-                onClick={() => setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id)}>
-                {stickers[thisMessage.content]}
+                onClick={() => {
+                  setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id);
+                  console.log(thisMessage, prevMessage, nextMessage);
+                }}>
+                {stickers[parseInt(thisMessage.content)]}
               </div>
             </div>
           )}
@@ -101,10 +117,10 @@ const MessageBubble: FC<Props> = ({
         className={`mb-1 flex flex-col items-center justify-center transition-[padding] duration-300 ${
           isClicked === thisMessage.id
             ? 'pb-4'
-            : nextMessage &&
-              nextMessage.sender !== 'You' &&
-              nextMessage.timestamp - thisMessage.timestamp > 1000 * 60 * 2 &&
-              'pb-2'
+            : (nextMessage &&
+                nextMessage.sender !== thisMessage.sender &&
+                nextMessage.timestamp - thisMessage.timestamp > 1000 * 60 * 2) ||
+              (nextMessage && nextMessage.sender !== thisMessage.sender && 'pb-2')
         }`}>
         <div
           className={`mx-auto ${
@@ -114,10 +130,10 @@ const MessageBubble: FC<Props> = ({
                 new Date(thisMessage.timestamp).toLocaleDateString()) ||
             isClicked === thisMessage.id ||
             !prevMessage
-              ? 'h-12'
+              ? 'h-10'
               : 'h-0 opacity-0'
           }
-           overflow-hidden transition-[height,opacity] duration-300`}>
+           flex items-center justify-center overflow-hidden transition-[height,opacity] duration-300`}>
           {new Date().toLocaleDateString() !== new Date(thisMessage.timestamp).toLocaleDateString()
             ? new Date(thisMessage.timestamp).getDate().toString().padStart(2, '0') +
               '/' +
@@ -135,7 +151,7 @@ const MessageBubble: FC<Props> = ({
           <div
             className={`mr-2 flex h-9 w-9 items-center justify-center ${
               (!nextMessage ||
-                nextMessage.sender === 'You' ||
+                nextMessage.sender !== thisMessage.sender ||
                 isClicked === thisMessage.id ||
                 nextMessage.timestamp - thisMessage.timestamp > 1000 * 60 * 15 ||
                 thisMessage.type !== 'text') &&
@@ -148,9 +164,9 @@ const MessageBubble: FC<Props> = ({
             <div
               className={`transition-all duration-300 ${
                 prevMessage &&
-                prevMessage.sender !== 'You' &&
+                prevMessage.sender === thisMessage.sender &&
                 thisMessage.timestamp - prevMessage.timestamp <= 1000 * 60 * 15 && // 15 mins
-                !(isClicked === thisMessage.id) &&
+                isClicked !== thisMessage.id &&
                 prevMessage.type === 'text' &&
                 isClicked !== prevMessage.id
                   ? 'rounded-tl-md'
@@ -158,24 +174,50 @@ const MessageBubble: FC<Props> = ({
               } 
             ${
               nextMessage &&
-              nextMessage.sender !== 'You' &&
+              nextMessage.sender === thisMessage.sender &&
               nextMessage.timestamp - thisMessage.timestamp <= 1000 * 60 * 15 && // 15 mins
-              !(isClicked === thisMessage.id) &&
+              isClicked !== thisMessage.id &&
               nextMessage.type === 'text' &&
               isClicked !== nextMessage.id
                 ? 'rounded-bl-md'
                 : 'rounded-bl-3xl'
             } 
             min-w-[40px] max-w-[400px] whitespace-pre-wrap break-words rounded-r-3xl bg-green-400 px-3 py-2 text-center`}
-              onClick={() => setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id)}>
+              onClick={() => {
+                setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id);
+                console.log(thisMessage, prevMessage, nextMessage);
+                // console.log(
+                //   prevMessage && 'prevMessage\n',
+                //   prevMessage &&
+                //     prevMessage.sender !== thisMessage.sender &&
+                //     'prevMessage.sender !== thisMessage.sender\n',
+                //   prevMessage &&
+                //     thisMessage.timestamp - prevMessage.timestamp <= 1000 * 60 * 15 &&
+                //     'thisMessage.timestamp - prevMessage.timestamp <= 1000 * 60 * 15 \n', // 15 mins
+                //   isClicked !== thisMessage.id && 'isClicked !== thisMessage.id\n',
+                //   prevMessage && prevMessage.type === 'text' && 'prevMessage.type === "text"\n',
+                //   prevMessage && isClicked !== prevMessage.id && 'isClicked !== prevMessage.id\n'
+                // );
+                console.log(prevMessage && prevMessage.sender !== thisMessage.sender);
+              }}>
               {thisMessage.content}
+              {prevMessage &&
+                prevMessage.sender !== thisMessage.sender &&
+                thisMessage.timestamp - prevMessage.timestamp <= 1000 * 60 * 15 && // 15 mins
+                isClicked !== thisMessage.id &&
+                prevMessage.type === 'text' &&
+                isClicked !== prevMessage.id &&
+                'top not round'}
             </div>
           ) : (
             <div className={`flex w-full justify-start text-6xl`}>
               <div
                 className="my-2"
-                onClick={() => setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id)}>
-                {stickers[thisMessage.content]}
+                onClick={() => {
+                  setIsClicked(isClicked === thisMessage.id ? '' : thisMessage.id);
+                  console.log(thisMessage, prevMessage, nextMessage);
+                }}>
+                {stickers[parseInt(thisMessage.content)]}
               </div>
             </div>
           )}
