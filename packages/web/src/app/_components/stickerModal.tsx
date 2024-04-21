2e08caffd252bcb85345bc451f2bd9e89a4bb226
punyaphatsura <punyaphat.sura@gmail.com>
2024-04-21 14:10:11 +0700

    feat: :sparkles: change cool sticker

--- packages/web/src/app/_components/stickerModal.tsx
+++ packages/web/src/app/_components/stickerModal.tsx
@@ -7,18 +7,18 @@ interface StickerModalProps {
   isVisible: boolean;
 }
 
-const stickers = ['🌟', '💬', '🎉', '👍']; // Example stickers, can be images or other components
+export const stickers = ['🌟', '💬', '🎉', '🗿']; // Example stickers, can be images or other components
 
 const StickerModal: React.FC<StickerModalProps> = ({ onSelectSticker, isVisible }) => {
   if (!isVisible) return null;
 
   return (
-    <div className="bg-project_gray absolute bottom-0 right-0 mb-12 ml-2 grid w-[150px] grid-cols-2 rounded-lg p-4 shadow-lg">
+    <div className="absolute bottom-0 right-0 mb-12 ml-2 grid w-[150px] grid-cols-2 gap-2 rounded-lg bg-project_gray p-4 shadow-lg">
       {stickers.map((sticker, index) => (
         <button
           key={index}
           onClick={() => onSelectSticker(index)}
-          className="hover:bg-project_light_gray rounded-xl p-2">
+          className="cursor-pointer rounded-xl p-2 transition-colors hover:bg-project_light_gray">
           <span className="text-3xl">{sticker}</span>
         </button>
       ))}
