d94f38e7a409d670eddff89032c4463adf3aba54
TitorPs360 <TitorPs360Gamer@gmail.com>
2024-04-22 05:34:17 +0700

    fix(Frontend): :bug: Redirect on main page

--- packages/web/src/app/(routes)/page.tsx
+++ packages/web/src/app/(routes)/page.tsx
@@ -1,11 +1,41 @@
 'use client';
 
+import { RealmContext } from '@/context/realm';
 import loadingAnimation from '@assets/lotties/loading.json';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import React, { useContext, useEffect } from 'react';
+
 const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
-import React from 'react';
 
 const Page = () => {
+  const Realm = useContext(RealmContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const login = async () => {
+      const username = localStorage.getItem('username');
+      const password = localStorage.getItem('password');
+
+      if (!username || !password) {
+        console.log('no username or password');
+        router.push('/login');
+        return;
+      }
+
+      await Realm.login(username, password);
+    };
+
+    login();
+  }, []);
+
+  useEffect(() => {
+    if (Realm.realm?.isLoggedIn) {
+      router.push('/chat');
+    }
+  }, [Realm.realm?.isLoggedIn]);
+
   return (
     <div className="flex h-screen w-full items-center justify-center bg-project_black">
       <Lottie animationData={loadingAnimation} />
